729eaeee33907cfe8ffefa35577e169288094ef9
jfgarcia0911 <jfgarcia0911@gmail.com>
2026-02-26 10:55:44 +0800

    feat: add Trade In section with ProductHero component
    - Created Trade In section using reusable ProductHero component
    - Fixed hydration error caused by <p> containing <div> by changing inner element to <span> in ProductHero
    - Updated styling for responsive layout and added Apple logo image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { motion } from "framer-motion";
 import Image from "next/image";
 import ProductHero from "./ProductHero";
 
@@ -53,9 +52,10 @@ export default function Hero() {
 			/>
 
 			<div className="md:flex gap-3 w-full ">
+				{/* Watch series 11 section */}
 				<ProductHero
 					title={
-						<h1 className="flex items-center gap-1 text-xl md:text-2xl lg:text-4xl font-bold">
+						<div className="flex items-center gap-1 text-xl md:text-2xl lg:text-4xl font-bold">
 							<Image
 								src="/apple-logo.png"
 								alt="Apple Watch Series 11 Ultra"
@@ -64,13 +64,13 @@ export default function Hero() {
 								className="h-6 lg:h-10 w-6 lg:w-10 mx-auto"
 							/>
 							WATCH <span className="font-normal ml-2">SERIES 11</span>
-						</h1>
+						</div>
 					}
 					subtitle={
-            <p className="text-xl">
-              The ultimate way to watch your health.
-            </p>
-          }
+						<div className="text-xl">
+							The ultimate way to watch your health.
+						</div>
+					}
 					imageDesktop="/watch-series11.png"
 					imageMobile="/watch-series11.png"
 					imageClassNameDesktop="md:w-120 lg:w-200 scale-130 translate-y-10"
@@ -80,9 +80,11 @@ export default function Hero() {
 					backgroundColor="bg-gray-100"
 					sectionHeight=" lg:h-150"
 				/>
+
+				{/* Wwatch Black Unity Band Section */}
 				<ProductHero
 					title={
-						<h1 className="flex items-center gap-1 text-xl md:text-2xl lg:text-4xl font-bold">
+						<div className="flex items-center gap-1 text-xl md:text-2xl lg:text-4xl font-bold">
 							<Image
 								src="/apple-logo.png"
 								alt="Apple Watch Series 11 Ultra"
@@ -90,40 +92,155 @@ export default function Hero() {
 								height={200}
 								className="h-6 lg:h-10 w-6 lg:w-10 mx-auto filter invert"
 							/>
-							WATCH 
-						</h1>
+							WATCH
+						</div>
 					}
 					subtitle={
-            <p className="text-xl leading-6">
-              The new Black Unity band. <br /> Inspired by the power of connection.
-            </p>
-          }
+						<div className="text-xl leading-6  ">
+							The new Black Unity band. Inspired by the power of connection.
+						</div>
+					}
 					imageDesktop="/black-band.png"
-					imageMobile="/watch-series11.png"
+					imageMobile="/black-band.png"
 					imageClassNameDesktop="md:w-120 lg:w-200 rotate-310 translate-y-5"
-					imageClassNameMobile="w-140 translate-y-14 scale-120"
+					imageClassNameMobile="w-140 translate-y-15 scale-120 rotate-310"
 					buttonLeftText="Shop"
-					buttonRightText="Buy"
+					buttonRightText=""
 					backgroundColor="bg-black"
 					sectionHeight=" lg:h-150"
-				dark={true}
-
+					dark={true}
 				/>
 			</div>
 
-			<ProductHero
-				title='MacBook Pro 14"'
-				subtitle="Supercharged by M5."
-				imageDesktop="/macbookPro.png"
-				imageMobile="/macbookPro.png"
-				imageClassNameDesktop="h-100 w-200"
-				imageClassNameMobile="h-88 w-160 -translate-y-13"
-				buttonLeftText="Learn more"
-				buttonRightText="Buy"
-				backgroundColor="bg-black"
-				sectionHeight="h-162"
-				dark={true}
-			/>
+			<div className="md:flex gap-3 w-full ">
+				{/* Ipad Pro */}
+				<ProductHero
+					title={
+						<div className="flex items-center gap-1 text-xl md:text-2xl lg:text-4xl font-bold">
+							iPad Pro
+						</div>
+					}
+					subtitle={
+						<div className="text-xl leading-6 px-10 ">
+							Advanced AI performance and game-changing capabilities.
+						</div>
+					}
+					imageDesktop="/ipad-pro.png"
+					imageMobile="/ipad-pro.png"
+					imageClassNameDesktop="md:w-55 lg:w-80  translate-y-0"
+					imageClassNameMobile="w-65 translate-y-0 "
+					buttonLeftText="Learn more"
+					buttonRightText="Buy"
+					backgroundColor="bg-black"
+					sectionHeight=" lg:h-150"
+					dark={true}
+				/>
+
+				{/* AirPods Pro3 Section  */}
+				<ProductHero
+					title={
+						<div className="flex items-center gap-1 text-xl md:text-2xl lg:text-4xl font-bold">
+							AirPods Pro 3
+						</div>
+					}
+					subtitle={
+						<div className="text-xl leading-6  ">
+							The world&apos;s best in-ear Active Noise Cancellation.
+						</div>
+					}
+					imageDesktop="/air-pads-pro3.png"
+					imageMobile="/air-pads-pro3.png"
+					imageClassNameDesktop="md:w-80 lg:w-110  translate-y-5"
+					imageClassNameMobile="w-80 translate-y-5 h-50"
+					buttonLeftText="Learn more"
+					buttonRightText="Buy"
+					backgroundColor="bg-gray-100"
+					sectionHeight=" lg:h-150"
+				/>
+			</div>
+
+			<div className="md:flex gap-3 w-full ">
+				{/* Trade In Section  */}
+				<ProductHero
+					title={
+						<div className="flex items-center gap-1 text-xl md:text-2xl lg:text-4xl font-bold">
+							<Image
+								src="/apple-logo.png"
+								alt="Apple Watch Series 11 Ultra"
+								width={200}
+								height={200}
+								className="h-6 lg:h-8 w-6 lg:w-8 mx-auto "
+							/>
+              Trade In
+						</div>
+					}
+					subtitle={
+						<div className="text-xl leading-6  px-10">
+							Get up to $180-$650 in credit when you trade in iPhone 13 or higher.
+						</div>
+					}
+					imageDesktop="/trade-in.png"
+					imageMobile="/trade-in.png"
+					imageClassNameDesktop=" w-full  -translate-y-10 md:h-60 lg:h-110"
+					imageClassNameMobile="w-80 translate-y-5 h-50"
+					buttonLeftText="Get your estimate"
+					buttonRightText=""
+					backgroundColor="bg-gray-100"
+					sectionHeight=" lg:h-150"
+				/>
+				{/* Card Section  */}
+				<ProductHero
+					title={
+						<div className="flex items-center gap-1 text-xl md:text-2xl lg:text-4xl font-bold">
+							<Image
+								src="/apple-logo.png"
+								alt="Apple Watch Series 11 Ultra"
+								width={200}
+								height={200}
+								className="h-6 lg:h-8 w-6 lg:w-8 mx-auto "
+							/>
+              Card
+						</div>
+					}
+					subtitle={
+						<div className="text-xl leading-6  px-10">
+							Get up to 3% Daily Cash back with every purchase.
+						</div>
+					}
+					imageDesktop="/card.png"
+					imageMobile="/card.png"
+					imageClassNameDesktop="md:w-80 lg:w-110  translate-y-5"
+					imageClassNameMobile="w-80 translate-y-5 h-50"
+					buttonLeftText="Learn more"
+					buttonRightText="Apply now"
+					backgroundColor="bg-gray-100"
+					sectionHeight=" lg:h-150"
+				/>
+			</div>
+
+
+      {/* Ipad Pro */}
+				<ProductHero
+					title={
+						<div className="flex items-center gap-1 text-xl md:text-2xl lg:text-4xl font-bold">
+							iPad Pro
+						</div>
+					}
+					subtitle={
+						<div className="text-xl leading-6 px-10 ">
+							Advanced AI performance and game-changing capabilities.
+						</div>
+					}
+					imageDesktop="/ipad-pro.png"
+					imageMobile="/ipad-pro.png"
+					imageClassNameDesktop="md:w-55 lg:w-80  translate-y-0"
+					imageClassNameMobile="w-65 translate-y-0 "
+					buttonLeftText="Learn more"
+					buttonRightText="Buy"
+					backgroundColor="bg-black"
+					sectionHeight=" lg:h-150"
+					dark={true}
+				/>
 		</main>
 	);
 }
--- components/ProductHero.tsx
+++ components/ProductHero.tsx
@@ -30,7 +30,7 @@ export default function ProductHero({
 }: ProductHeroProps) {
 	return (
 		<section
-			className={`relative flex flex-col items-center w-full  h-125 md:${sectionHeight} pt-7 md:pt-13 mb-3
+			className={`relative flex flex-col items-center  w-full  h-125 md:${sectionHeight} pt-7 md:pt-13 mb-3
         ${backgroundColor}
         ${dark ? " text-white" : "text-gray-900"}
         `}
@@ -46,14 +46,14 @@ export default function ProductHero({
 			</motion.h1>
 
 			{/* Subheadline */}
-			<motion.p
+			<motion.div
 				initial={{ opacity: 0, y: 20 }}
 				animate={{ opacity: 1, y: 0 }}
 				transition={{ duration: 0.8, delay: 0.15 }}
-				className="mt-1 text-center  max-w-48 md:max-w-xl lg:max-w-2xl text-lg md:text-2xl   leading-tight"
+				className="mt-1 text-center   md:max-w-xl lg:max-w-2xl text-lg md:text-2xl   leading-tight"
 			>
 				{subtitle}
-			</motion.p>
+			</motion.div>
 
 			{/* CTA */}
 			<motion.div
@@ -62,10 +62,11 @@ export default function ProductHero({
 				transition={{ delay: 0.3 }}
 				className="mt-4 flex gap-4"
 			>
-				<a className="rounded-full bg-blue-600 px-3 md:px-6 py-1 md:py-2 text-md md:text-lg  hover:bg-blue-500 transition text-white">
+        
+				<a className={`rounded-full bg-blue-600 px-3 md:px-6 py-1 md:py-2 text-md md:text-lg  hover:bg-blue-500 transition text-white `}>
 					{buttonLeftText}
 				</a>
-				<a className="rounded-full border   text-blue-500 hover:bg-blue-600 hover:text-white transition px-3 md:px-6 py-1 md:py-2 text-md md:text-lg">
+				<a className={`rounded-full border   text-blue-500 hover:bg-blue-600 hover:text-white transition px-3 md:px-6 py-1 md:py-2 text-md md:text-lg ${buttonRightText == "" ? "hidden" : "" }`}>
 					{buttonRightText}
 				</a>
 			</motion.div>
@@ -75,7 +76,7 @@ export default function ProductHero({
 				initial={{ opacity: 0, y: -30 }}
 				animate={{ opacity: 1, y: 0 }}
 				transition={{ duration: 1, delay: 0.4 }}
-				className=" mt-10 md:mt-8 w-full  h-77 md:h-150   mx-auto relative overflow-hidden"
+				className=" mt-10 md:mt-8 w-full  h-82 md:h-150   mx-auto relative overflow-hidden"
 			>
 				<Image
 					src={imageDesktop}
